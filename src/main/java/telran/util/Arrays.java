87f04373b483634cedf7fc199aa46a8b5187fd3c
JuliaDavidovich <julia_davidovich@mail.ru>
2024-07-15 17:40:49 +0300

    HW - Functions bynarySearch, insertSorted, isOneSwap

--- src/main/java/telran/util/Arrays.java
+++ src/main/java/telran/util/Arrays.java
@@ -24,11 +24,11 @@ public class Arrays {
      * @return reference to a new array containing @param number at @param index
      */
     public static int[] insert(int[] ar, int index, int number) {
-        int[] res = new int [ar.length + 1];
+        int[] res = new int[ar.length + 1];
         System.arraycopy(ar, 0, res, 0, index);
         res[index] = number;
         System.arraycopy(ar, index, res, index + 1, ar.length - index);
-        return res;//10, 7, 12, 41, -4, 13, 3, 14
+        return res;// 10, 7, 12, 41, -4, 13, 3, 14
         // TODO
         // creates new array with all elements from the given "ar" and
         // the given "number" at the given index
@@ -42,22 +42,25 @@ public class Arrays {
      * @return new array with no removed from @param numbers number at @param index
      */
     public static int[] remove(int[] numbers, int index) {
-        int[] res = new int [numbers.length - 1];
+        int[] res = new int[numbers.length - 1];
         System.arraycopy(numbers, 0, res, 0, index);
-        System.arraycopy(numbers, index + 1, res, index, numbers.length - index -1);
+        System.arraycopy(numbers, index + 1, res, index, numbers.length - index - 1);
         return res;
         // TODO
         // creates new array with no element in "numbers" at "index"
         // to apply System.arraycopy method
     }
 
-    //Bubble sort
-    public static void pushMaxAtEnd(int [] ar) {
-        for(int i = 1; i < ar.length; i++) {
-            if (ar[i] < ar[i - 1]) {
-                swap(ar, i, i - 1);
+    // Bubble sort
+    private static boolean pushMaxAtEnd(int[] ar, int length) {
+        boolean res = true;
+        for (int i = 0; i < length; i++) {
+            if (ar[i] > ar[i + 1]) {
+                res = false;
+                swap(ar, i, i + 1);
             }
         }
+        return res;
     }
 
     private static void swap(int[] ar, int i, int j) {
@@ -66,9 +69,69 @@ public class Arrays {
         ar[j] = tmp;
     }
 
-    public static void sort(int [] ar) {
-        for(int i = 0; i < ar.length; i ++) {
-            pushMaxAtEnd(ar);
+    public static void sort(int[] ar) {
+        int length = ar.length;
+        boolean flSorted = false;
+        while (!flSorted) {
+            length--;
+            flSorted = pushMaxAtEnd(ar, length);
         }
+    }
+
+    public static int binarySearch(int[] ar, int key) {
+        int l = 0;
+        int r = ar.length - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (ar[mid] == key) {
+                return mid;
+            }
+            if (key < ar[mid]) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -l - 1;
+    }
+
+    public static int[] insertSorted(int[] arSorted, int number) {
+        int index = (binarySearch(arSorted, number) + 1) * -1;
+        insert(arSorted, index, number);
+        return arSorted;
+    }
+
+    public static boolean isOneSwap(int[] array) {
+        int count = 0;
+        boolean res = false;
+        int index1 = -1;
+        int index2 = -1;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                if (index1 == -1) {
+                    index1 = i;
+                }
+                index2 = i + 1;
+                count++;
+            }
+        }
+        if (count <= 2 && count != 0) {
+            for(int i = 0; i < array.length - 1; i++){
+                if(array[i] == array[i + 1]){
+                    index2 = i + 1;
+                }
+            }
+            if (index1 != index2 - 1) {
+                if ((index1 != 0 && array[index1 - 1] <= array[index2] && array[index2] <= array[index1 + 1]
+                        || index1 == 0 && array[index2] <= array[index1 + 1])
+                        && (index2 != array.length - 1 && array[index2 - 1] <= array[index1]
+                                && array[index1] <= array[index2 + 1]                        
+                                ||index2 == array.length - 1
+                                && array[index1] >= array[index2 - 1])) {
+                    res = true;
+                }
+            }
+        }
+        return res;
     }
 }
--- src/test/java/telran/util/test/ArraysTest.java
+++ src/test/java/telran/util/test/ArraysTest.java
@@ -3,9 +3,13 @@ package telran.util.test;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.abort;
 import static telran.util.Arrays.*;
 
+import java.util.Random;
+
 public class ArraysTest {
+private static final int N_ELEMENTS = 1000;
 int[] numbers = {10, 7, 12, -4, 13, 3, 14};
 @Test 
 void searchTest(){
@@ -45,20 +49,68 @@ void removeTest(){
 }
 
 @Test
-void pushMaxAtEndTest(){
-    int[] testNumbers = java.util.Arrays.copyOf(numbers, numbers.length);
-    pushMaxAtEnd(testNumbers);
-    assertEquals(14, testNumbers[testNumbers.length - 1]);
-    assertEquals(13, testNumbers[testNumbers.length - 2]);
-    testNumbers = new int[] {20, -10, 10, -17};
-    pushMaxAtEnd(testNumbers);
-}
-
-@Test
 void sortTest() {
     int[] testNumbers = java.util.Arrays.copyOf(numbers, numbers.length);
     int [] expected = {-4, 3, 7, 10, 12, 13, 14};
     sort(testNumbers);
     assertArrayEquals(expected, testNumbers);
+}
+
+@Test
+void sortTestRandomArray(){
+    int[] array = getRandomArray(N_ELEMENTS);
+    int limit = array.length - 1;
+    sort(array);
+    for(int i = 0; i < limit; i++){
+        assertTrue(array[i] <= array[i + 1]);
+    }
+}
+private int[] getRandomArray(int nElements) {
+    int[] res = new int[nElements];
+    Random random = new Random();
+    for(int i = 0; i < nElements; i++) {
+        res[i] = random.nextInt();
+    }
+    return res;
+}
+
+@Test
+void binarySearchTest() {
+    int[] ar = {1, 3, 5, 6, 87, 98};
+    assertEquals(2, binarySearch(ar, 5));
+    assertEquals(0, binarySearch(ar, 1));
+    assertEquals(5, binarySearch(ar, 98));
+    assertEquals(-1, binarySearch(ar, 50));
+}
+
+@Test
+void insertSortedTest() {
+    int[] ar = {1, 3, 5, 6, 87, 98};
+    int [] expected = {1, 3, 5, 6, 10, 87, 98};
+    assertArrayEquals(expected, insert(ar, 4, 10));
+    int [] expected1 = {0, 1, 3, 5, 6, 87, 98};
+    assertArrayEquals(expected1, insert(ar, 0, 0));
+    int [] expected2 = {1, 3, 5, 6, 87, 98, 101};
+    assertArrayEquals(expected2, insert(ar, 6, 101));
+}
+
+@Test
+void isOneSwapTest() {
+    int[] ar = {1, 2, 5, 4, 3, 6}; // one swap
+    assertTrue(isOneSwap(ar));
+    int[] ar1 = {1, 2, 5, 4, 3, 6, 22, 45, 12}; //more one swap
+    assertFalse(isOneSwap(ar1));
+    int[] ar2 = {-4, 13, 7, 10, 12, 8, 14}; //more one swap
+    assertFalse(isOneSwap(ar2));
+    int[] ar3 = {1, 2, 4, 3, 5, 6}; // one swap neibours
+    assertFalse(isOneSwap(ar3));
+    int[] ar4 = {6, 2, 3, 4, 5, 1}; // one swap last elements
+    assertTrue(isOneSwap(ar4)); 
+    int[] ar5 = {1, 2, 3, 6, 3, 5, 3}; // 
+    assertTrue(isOneSwap(ar5)); 
+    int[] ar6 = {1, 2, 5, 3, 3, 3, 6}; // 
+    assertTrue(isOneSwap(ar6));
+    int[] ar7 = {1, 1, 1, 2, 5, 3, 3, 3, 6}; // 
+    assertTrue(isOneSwap(ar7));
 }
 }
